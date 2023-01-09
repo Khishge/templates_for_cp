d9ecda5bcceb29a928ebed35f662d6e5227a8dea
Khishge <67745053+Khishge@users.noreply.github.com>
2023-01-09 20:46:31 +0800

    Add files via upload

---
+++ Sparse_Table.cpp
@@ -0,0 +1,26 @@
+template<typename T>
+struct Sparse_Table {
+	int lgflr(int x) {
+		return 31 - __builtin_clz(x);
+	}
+	T f(T x, T y) { // implement !!!
+		return min(x, y);
+	}
+	int N, K;
+	vector<vector<T>> st;
+	void init(vector<T> a) {
+		N = (int) a.size();
+		K = lgflr(N);
+		st = vector<vector<T>>(K + 1, vector<T>(N));
+		swap(st[0], a);
+		for(int i = 1; i <= K; ++i) {
+			for(int j = 0; j + (1 << i) <= N; ++j) {
+				st[i][j] = f(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+			}
+		}
+	}
+	T query(int L, int R) {
+		int i = lgflr(R - L + 1);
+		return f(st[i][L], st[i][R - (1 << i) + 1]);
+	}
+};
