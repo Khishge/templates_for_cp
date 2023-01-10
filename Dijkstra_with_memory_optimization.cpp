60ffc035c871ddc3f02ade0f145cdb8d57944657
Khishge <67745053+Khishge@users.noreply.github.com>
2023-01-11 02:01:20 +0800

    Create Dijkstra_with_memory_optimization.cpp

---
+++ Dijkstra_with_memory_optimization.cpp
@@ -0,0 +1,36 @@
+const long long INF = 0x3f3f3f3f3f3f3f3f;
+
+void Dijkstra(int st, vector<long long>& D, vector<int>& P) {
+	int n = (int) size(adj);
+	D.resize(n, INF); // make sure to declare larger #INF# for long long
+	P.resize(n, -1);
+	auto cmp = [&D] (const int& a, const int& b) -> bool { return (D[a] < D[b] || (D[a] == D[b] && a < b)); };
+	set<int, decltype(cmp)> s(cmp);
+	D[st] = 0;
+	s.insert(st);
+	while(!s.empty()) {
+		int cur = *begin(s);
+		s.erase(begin(s));
+		for( u : adj[cur]) {
+			int to = u.first;
+			long long len = u.second;
+			if(D[cur] + len < D[to]) {
+				s.erase(to);
+				D[to] = D[cur] + len;
+				P[to] = cur;
+				s.insert(to);
+			}
+		}
+	}
+}
+
+vector<int> Restore_Path(int st, int en, vector<int>& P) {
+	vector<int> path;
+	while(en != st) {
+		path.push_back(en);
+		en = P[en];
+	}
+	path.push_back(st);
+	reverse(begin(path), end(path));
+	return path;
+}
